2ab324c37a4a4611569eb64c9bd18225c1537efe
bleblablue <son14102007@gmail.com>
2026-05-18 17:16:37 +0700

    them login vao giao tiep giua server va client

--- Client.py
+++ Client.py
@@ -1,16 +1,22 @@
 import socket
 # 1. khai bao host va port cua server
 host="127.0.0.1" #ip cua server
-port=2222 # port cua server
+port=3333 # port cua server
 #2. open socket (ipv4,tcp)
 client_socket= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #3. connect to server
 client_socket.connect((host, port));
-#4. send data
-mess="siuuuuu"
-client_socket.sendall(mess.encode("utf-8"))
-#5.nhan du lieu tu server
-data=client_socket.recv(200)
-print("nhan tu server: ", data.decode("utf-8"))
-#6.close
-client_socket.close()   
+#4. nhan tu server
+data=client_socket.recv(1024)
+print(data.decode())
+#user nhap
+username=input()
+#5.gui len server
+client_socket.send(username.strip().encode())
+#6. pw tuong tu
+data=client_socket.recv(1024)
+print(data.decode())
+password=input()
+client_socket.send(password.strip().encode())
+#7.close
+client_socket.close()
--- Server.py
+++ Server.py
@@ -2,12 +2,21 @@ import socket
 #1. socket
 server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #2. lang nghe tai ip va port
-server_socket.bind(("0.0.0.0",2222))
+server_socket.bind(("0.0.0.0",3333))
 server_socket.listen(5)
 print("dang cho ket noi...")
 while True:
     client_socket,address=server_socket.accept()
-    print("ket noi tu: {address}")
+    print(f"ket noi tu: {address}")
     #3.data to client
-    client_socket.send("connect successfull".encode("utf-8"))
-    client_socket.close
+    client_socket.send("login\nusername".encode("utf-8"))
+        #nhan username
+    username=client_socket.recv(1024).decode()
+        #gui password prompt
+    client_socket.send("password: ".encode())
+        #nhan pw
+    password=client_socket.recv(1024).decode()
+    #print log
+    print(username)
+    print(password)
+    client_socket.close()
