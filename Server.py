bc851f8b2c1f91cc67d95320fbd6d58f7ed648f9
bleblablue <son14102007@gmail.com>
2026-05-19 15:10:57 +0700

    them thread de co the handle nhieu client cung luc

--- Server.py
+++ Server.py
@@ -1,33 +1,51 @@
 import socket
 from datetime import datetime
+import threading
+#1. ham xu ly thread
+def handle_client(client_socket,address):
+    try:
+        #.data to client
+        client_socket.send("login\nusername".encode("utf-8"))
+            #nhan username
+        username=client_socket.recv(1024).decode().strip()
+            #gui password prompt
+        client_socket.send("password: ".encode())
+            #nhan pw
+        password=client_socket.recv(1024).decode().strip()
+        #print log
+        print(f"USERNAME: {username}")
+        print(f"PASSWORD: {password}")
+            #them thoi gian vao log
+        time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open("attacker.log","a") as f:
+            f.write(f"IP: {address}\n")
+            f.write(f"USERNAME: {username}\n")
+            f.write(f"PASSWORD: {password}\n")
+            f.write(f"TIME: {time}\n")
+            f.write("--------------------------------------\n")
+    except Exception as e:
+        print(e)
+    finally:
+        client_socket.close()
+    return ;
 
-#1. socket
+
+#2. socket
 server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#2. lang nghe tai ip va port
+#3. lang nghe tai ip va port
 server_socket.bind(("0.0.0.0",3333))
 server_socket.listen(5)
 print("dang cho ket noi...")
 while True:
     client_socket,address=server_socket.accept()
     print(f"ket noi tu: {address}")
-    #3.data to client
-    client_socket.send("login\nusername".encode("utf-8"))
-        #nhan username
-    username=client_socket.recv(1024).decode()
-        #gui password prompt
-    client_socket.send("password: ".encode())
-        #nhan pw
-    password=client_socket.recv(1024).decode()
-    #print log
-    print(f"USERNAME: {username}")
-    print(f"PASSWORD: {password}")
-        #them thoi gian vao log
-    time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open("attacker.log","a") as f:
-        f.write(f"IP: {address}\n")
-        f.write(f"USERNAME: {username}\n")
-        f.write(f"PASSWORD: {password}\n")
-        f.write(f"TIME: {time}\n")
-        f.write("--------------------------------------\n")
-    client_socket.close()
-
+#4. tao thread va chay
+    thread=threading.Thread(
+        target=handle_client,
+        args=(client_socket,address)
+    )
+    thread.daemon=True #neu main process chet thread chet theo
+    thread.start()
+    
+    
+    
